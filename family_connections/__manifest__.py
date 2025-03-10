d22ada5406b8289901f163eb5d5ae50fcebf76fb
[Adrián Kovács] <[kovacs.adrian351@gmail.com]>
2025-03-10 14:19:49 +0100

    family_connections module initiated

---
+++ family_connections/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Family Connections in Contacts',
+    'version': '1.0.0',
+    'summary': 'Adds in the option to connect contacts to a contact indicating the familial relation between contacts.',
+    'category': 'Custom',
+    'author': 'Adrian',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': ['views/res_partner_view.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
