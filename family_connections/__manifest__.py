dc87a2a8455fd69f163f4edd88c7a16f87b16298
[Adrián Kovács] <[kovacs.adrian351@gmail.com]>
2025-03-13 18:04:29 +0100

    family_connections second iteration (installable and mostly well functioning for the first time)

--- family_connections/__manifest__.py
+++ family_connections/__manifest__.py
@@ -1,14 +1,21 @@
 {
-    'name': 'Family Connections in Contacts',
-    'version': '1.0.0',
-    'summary': 'Adds in the option to connect contacts to a contact indicating the familial relation between contacts.',
-    'category': 'Custom',
-    'author': 'Adrian',
-    'license': 'LGPL-3',
-    'depends': ['contacts'],
-    'data': ['views/res_partner_view.xml', 
-             'security/ir.model.access.csv'],
-    'installable': True,
-    'application': True,
-    'auto_install': False
+    "name": "Family Connections in Contacts",
+    "version": "1.0.0",
+    "summary": "Adds familial relationship management in Contacts.",
+    "category": "Custom",
+    "author": "Adrian",
+    "license": "LGPL-3",
+    "depends": ["contacts"],
+    "data": [
+        # 1) Security rules first
+        "security/ir.model.access.csv",
+        # 2) Child model view (for res.partner.family.relation)
+        "views/res_partner_family_relation_view.xml",
+        # 3) The parent form inheritance (res.partner)
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
+
