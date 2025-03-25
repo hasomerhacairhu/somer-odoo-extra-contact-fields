1a09c73ef1e0db8f030521fa82c403282c51576a
[Adrián Kovács] <[kovacs.adrian351@gmail.com]>
2025-03-25 12:44:17 +0100

    Relationship típusok kompaktá tétele

--- family_connections/models/res_partner_relation.py
+++ family_connections/models/res_partner_relation.py
@@ -1,6 +1,6 @@
 # +TODO: időnként bug új felvitelnél a family relations-ben, 
 # +TODO: JSON fájl-ba helyezni a kapcsolati típusokat és a reciprocal_map-et
-# TODO: Megmarad:- gyermek- szülő- testvér- grandparent- unokatestvér- unoka- other (MIND ANGOLUL)
+# +TODO: Megmarad:- gyermek- szülő- testvér- grandparent- unokatestvér- unoka- other (MIND ANGOLUL)
 # TODO: Stakeholder: Multiselect Dropdown field!
 # TODO: BirthDate mező címkéje Date of Birth!
 # TODO: Alsó fülek: Sales and Purchases tűnjön el!
